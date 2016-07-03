fec9b7effee88befb86048a4368e182bb5db7840
mrZhang <zhangguoyong010@163.com>
2016-07-03 23:01:53 +0800

    from ubuntu

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy();
 def create_app():
     app=Flask(__name__)
     from .authority import authorityBp as authority_blueprint
     app.register_blueprint(authority_blueprint)
     from flask_bootstrap import Bootstrap
     bootstrap = Bootstrap(app)
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:vgelab010@115.29.110.148:5432/appDB'
+    db.init_app(app)
     return app
 
--- app/manage.py
+++ app/manage.py
@@ -1,6 +1,9 @@
 from app import create_app
+from app import  db
+from model.NavModel import User
 
 app=create_app()
 
 if __name__=='__main__':
     app.run("127.0.0.1",5000)
+
